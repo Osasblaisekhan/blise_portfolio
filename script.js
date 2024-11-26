e7f1b06c743df21b1e029c36097687230055826e
osasblaisekhan <blaisekhan204@gmail.com>
2024-11-26 06:48:00 -0800

    some_files_where_change

--- script.js
+++
@@ -1,6 +0,0 @@
-const mobile_menu = document.getElementById("mobile_menu");
-mobile_menu.addEventListener("click", function(){
-    const desktop_menu = document.getElementById("desktop_menu");
-    desktop_menu.classList.toggle("active");
-    mobile_menu.classList.toggle("active");
-})
